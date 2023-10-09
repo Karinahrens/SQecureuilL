0feef0ff8b5b6af9b1c0925670d68bae43163335
Abdulrahman <abdulx9999@gmail.com>
2023-10-09 12:08:54 +0100

    Updated Models,

--- backEnd/database/setup.sql
+++ backEnd/database/setup.sql
@@ -5,9 +5,11 @@ DROP TABLE IF EXISTS user_account;
 CREATE TABLE post (
     post_id INT GENERATED ALWAYS AS IDENTITY,
     user_id INT NOT NULL,
-    title VARCHAR (100) NOT NULL,
-    content VARCHAR (500) NOT NULL,
-    votes INT NOT NULL,
+    post_title VARCHAR (100) NOT NULL,
+    post_content VARCHAR (500) NOT NULL,
+    post_date DATE NOT NULL,
+    post_votes INT NOT NULL,
+    post_categories VARCHAR (100) NOT NULL,
     post_Status VARCHAR (100) NOT NULL,
     PRIMARY KEY (post_id)
     FOREIGN KEY (user_id) REFERENCES user_account("user_id")
