f615a9bd294312866cca2b6caf486df155ec8f49
karinahrens <karina.ahrens@gmail.com>
2023-10-11 16:44:20 +0100

    fixing the stage/status function

--- backEnd/routers/posts.js
+++ backEnd/routers/posts.js
@@ -9,10 +9,10 @@ postsRouter.get("/order", postsController.orderBy);
 postsRouter.get("/Category/:id", postsController.indexCategory);
 postsRouter.get("/Category/:id/Date", postsController.indexCategoryDate);
 postsRouter.get("/Category/:id/Vote", postsController.indexCategoryVote);
-postsRouter.get("/Category/:id/Status", postsController.indexCategoryStatus);
+postsRouter.get("/Category/:id/Stage", postsController.indexCategoryStage);
 postsRouter.get("/Date", postsController.indexDate);
 postsRouter.get("/Vote", postsController.indexVote);
-postsRouter.get("/Status", postsController.indexStatus);
+postsRouter.get("/Stage", postsController.indexStage);
 postsRouter.get("/:id", postsController.show);
 
 postsRouter.post("/", postsController.create);
