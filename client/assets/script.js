ef196f8cee137a8ab0a5b9c08b747b806e8f1825
Abdulrahman <abdulx9999@gmail.com>
2023-10-12 15:33:17 +0100

    Created Testing File for Front End

--- client/assets/script.js
+++ client/assets/script.js
@@ -1,258 +1,100 @@
 const API_ENDPOINT = 'https://backendsqecureuill.onrender.com/';
-
-
-function performLogin() {
-    alert("You've successfully logged in!")
-}
-
-function showRegistration() {
-    alert("Redirecting to registration page...")
-}
-
-let posts = []; 
-
-let currentPostIndex; 
-
 document.addEventListener("DOMContentLoaded", function() {
     const postsSection = document.querySelector('.posts-section');
-
-    const filters = document.querySelectorAll('[name="sort-order"]');
-
     const categoryFilter = document.querySelector('#category-filter');
-
+    const createPostForm = document.getElementById("createPostForm"); // Moved here
+    let posts = [];
+    let currentPostIndex;
     function fetchAndDisplay(endpoint) {
-        console.log("Fetching from:", endpoint);
         fetch(endpoint)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`HTTP error! Status: ${response.status}`);
-                }
-                return response.json();
-            })
+            .then(response => response.json())
             .then(data => {
                 posts = data;
                 displayPosts(posts);
             })
-            .catch(error => {
-                console.log("Error fetching posts: ", error);
-            });
+            .catch(error => console.log("Error fetching posts: ", error));
     }
-
-    document.getElementById("filterButton").addEventListener('click', applyFilter);
-
-    function applyFilter() {
-        const category = categoryFilter.value;
-        const sortOrder = document.querySelector('[name="sort-order"]:checked').value;
-    
-        let endpoint = `${API_ENDPOINT}posts`; // default
-    
-        if (category !== 'all') {
-            endpoint = `${API_ENDPOINT}posts/order?category=${category}&sort=${sortOrder}`;
-
-        } else {
-            endpoint = `${API_ENDPOINT}posts/${sortOrder}`
-        }
-    
-        fetchAndDisplay(endpoint);
+    function getPostClassByCategory(category) {
+        // Simplify and add missing categories if necessary
+        return category.toLowerCase().replace(' ', '-');
     }
-
-    // Initial fetch and display
-    fetchAndDisplay(`${API_ENDPOINT}posts`);
-
     function displayPosts(postsToDisplay) {
         let postsHTML = "";
         postsToDisplay.forEach(post => {
-            postsHTML += `<div class="post" data-post-id="${post.id}">
-                <h3>${post.title}</h3>
-                <h4>${post.stage}</h4>
-                <p>${post.content}</p>
-                <p>Category: ${post.categories}</p>
-                <p>Date: ${new Date(post.date).toLocaleDateString()}</p>
-                <p>Votes: ${post.votes}</p>
-
-            </div>`;
+            const postClass = getPostClassByCategory(post.categories);
+            postsHTML += `
+                <div class="post ${postClass}" data-post-id="${post.id}">
+                    <h3>${post.title}</h3>
+                    <p>${post.content}</p>
+                    <p>Category: ${post.categories}</p>
+                    <p>Date: ${new Date(post.date).toLocaleDateString()}</p>
+                    <p>Votes: ${post.votes}</p>
+                </div>`;
         });
         postsSection.innerHTML = postsHTML;
     }
-
-    const modal = document.getElementById("postModal");
-    const closeModal = document.querySelector(".close");
-
+    document.getElementById("filterButton").addEventListener('click', applyFilter);
+    function applyFilter() {
+        const category = categoryFilter.value;
+        const sortOrder = document.querySelector('[name="sort-order"]:checked').value;
+        let endpoint = `${API_ENDPOINT}posts`;
+        if (category !== 'all') {
+            endpoint += `/order?category=${category}&sort=${sortOrder}`;
+        } else {
+            endpoint += `/${sortOrder}`;
+        }
+        fetchAndDisplay(endpoint);
+    }
+    // Initial fetch and display
+    fetchAndDisplay(`${API_ENDPOINT}posts`);
+    // Post Details Modal
+    const postModal = document.getElementById("postModal");
+    const closeModal = postModal.querySelector(".close");
     postsSection.addEventListener('click', function(e) {
         const postElement = e.target.closest('.post');
         if (postElement) {
             currentPostIndex = Array.from(postsSection.children).indexOf(postElement);
             const post = posts[currentPostIndex];
-            document.getElementById("modalTitle").textContent = post.title;
-            document.getElementById("modalContent").textContent = post.content;
-            modal.style.display = "block";
+            postModal.querySelector("#modalTitle").textContent = post.title;
+            postModal.querySelector("#modalContent").textContent = post.content;
+            postModal.style.display = "block";
         }
     });
-
-    closeModal.onclick = () => {
-        modal.style.display = "none";
-    };
-
-    window.onclick = (event) => {
-        if (event.target === modal) {
-            modal.style.display = "none";
-        }
-    };
-
-    document.getElementById("voteBtn").addEventListener("click", () => {
-        if (typeof currentPostIndex !== 'undefined') {
-            const currentPost = posts[currentPostIndex];
-            const postId = currentPost.id; // Assuming your post objects have an 'id' property.
-    
-            // Make a PATCH request to the backend
-            fetch(`${API_ENDPOINT}posts/${postId}/Vote`, {
-                method: "PATCH",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ post_votes: 1 }), // Increment by 1
-            })
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error(`HTTP error! Status: ${response.status}`);
-                    }
-                    return response.json();
-                })
-                .then(updatedPost => {
-                    currentPost.votes = updatedPost.votes;
-                    alert("Thanks for voting!");
-                    displayPosts(posts);
-                })
-                .catch(error => {
-                    console.error("Error updating votes: ", error);
-                });
-        }
-    });
-    document.getElementById("downVoteBtn").addEventListener("click", () => {
-        if (typeof currentPostIndex !== 'undefined') {
-            const currentPost = posts[currentPostIndex];
-            const postId = currentPost.id;
-    
-            // Make a PATCH request to the backend to decrease the vote
-            fetch(`${API_ENDPOINT}posts/${postId}/downVote`, {
-                method: "PATCH",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ post_votes: -1 }), // Decrement by 1
-            })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`HTTP error! Status: ${response.status}`);
-                }
-                return response.json();
-            })
-            .then(updatedPost => {
-                currentPost.votes = updatedPost.votes;
-                alert("Vote removed!");
-                displayPosts(posts);
-            })
-            .catch(error => {
-                console.error("Error removing votes: ", error);
-            });
-        }
-    });
-    
-
-    const openPostModalBtn = document.getElementById("openPostModalBtn");
+    closeModal.onclick = () => postModal.style.display = "none";
+    window.onclick = (event) => { if (event.target === postModal) postModal.style.display = "none"; };
+    // Create Post Modal
     const createPostModal = document.getElementById("createPostModal");
-    const closeCreate = document.querySelector(".close-create");
-    
-    openPostModalBtn.addEventListener("click", function() {
-        createPostModal.style.display = "block";
+    document.getElementById("openPostModalBtn").addEventListener("click", () => createPostModal.style.display = "block");
+    createPostModal.querySelector(".close-create").onclick = () => createPostModal.style.display = "none";
+    // Submit Create Post Form
+    createPostForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+        // ... your existing form submission code ...
     });
-
-    closeCreate.onclick = function() {
-        createPostModal.style.display = "none";
-    };
-});
-
-createPostForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    const title = document.getElementById("postTitle").value;
-    const content = document.getElementById("postContent").value;
-    const category = document.getElementById("postCategory").value;
-
-    const currentDate = new Date();
-    const dateOptions = {
-        year: "numeric",
-        month: "2-digit",
-        day: "2-digit",
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
-        hour12: false,
-    };
-    const localDateTime = currentDate.toLocaleString("en-GB", dateOptions);
-
-    const postData = {
-        post_title: title,
-        post_content: content,
-        post_date: localDateTime,
-        post_categories: category,
-        post_votes: 0,
-        post_stage: 'Active'
-    };
-
-    fetch(`${API_ENDPOINT}posts`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(postData),
-    })
-        .then(response => {
-            if (!response.ok) {
-                console.error(`HTTP error! Status: ${response.status}`);
-                return response.text(); 
-            }
-            return response.json();
-        })
-        .then(data => {
-            if (data) {
-                console.log("Server response:", data);
-                createPostModal.style.display = "none";
-
-                posts.push(data);
-                displayPosts(posts);
-
-                document.getElementById("postTitle").value = "";
-                document.getElementById("postContent").value = "";
-                document.getElementById("postCategory").value = "Graffiti";
-            }
-        })
-        .catch(error => {
-            console.error("Error posting data to the server:", error);
-        });
-});
-
-document.getElementById('logout').addEventListener('click', () => {
-    //localStorage.removeItem('token');
-    
-    localStorage.clear(); 
-    window.location.assign('./login.html')
-})
-
-
-async function loadPosts () {
-
-    const options = {
-        headers: {
-            'Authorization': localStorage.getItem("token")
+    // Delete Post
+    document.getElementById("deleteBtn").addEventListener("click", function() {
+        if (typeof currentPostIndex !== 'undefined') {
+            const postIdToDelete = posts[currentPostIndex].post_id;
+            fetch(`${API_ENDPOINT}posts/${postIdToDelete}`, { method: "DELETE" })
+                .then(response => {
+                    if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+                    alert("Post deleted successfully!");
+                    window.location.href = "/client/home.html";
+                })
+                .catch(error => console.error("Error deleting the post:", error));
         }
+    });
+    // Logout
+    document.getElementById('logout').addEventListener('click', () => {
+        localStorage.clear();
+        window.location.assign('./login.html');
+    });
+    async function loadPosts() {
+        const options = {
+            headers: { 'Authorization': localStorage.getItem("token") }
+        };
+        const response = await fetch(`${API_ENDPOINT}posts`, options);
+        if (!options.headers.Authorization) (window.location.assign("./login.html"));
     }
-    const response = await fetch(`${API_ENDPOINT}/posts`, options);
-    //console.log(options.headers.Authorization)
-    if (options.headers.Authorization) (console.log("You've successfully logged in!"))
-    else {
-        window.location.assign("./login.html");
-    }
-}
-
-loadPosts();
+    loadPosts();
+});
