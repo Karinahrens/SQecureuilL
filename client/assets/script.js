644bd661451fe6cbe55b96f4a037910e52a55c93
James Threadgill <jpgthreadgill97@gmail.com>
2023-10-12 15:49:33 +0100

    Added styling to home page and moved create post to top left of navbar plus some extra bits

--- client/assets/script.js
+++ client/assets/script.js
@@ -1,41 +1,51 @@
 const API_ENDPOINT = 'https://backendsqecureuill.onrender.com/';
 
+let posts = [];
+let currentPostIndex;
 
 function performLogin() {
-    alert("You've successfully logged in!")
+    alert("You've successfully logged in!");
 }
 
 function showRegistration() {
-    alert("Redirecting to registration page...")
+    alert("Redirecting to registration page...");
 }
 
-let posts = []; 
+function getPostClassByCategory(category) {
+    return category.toLowerCase().replace(/ /g, '-');
+}
 
-let currentPostIndex; 
-
+function displayPosts(postsToDisplay) {
+    const postsSection = document.querySelector('.posts-section');
+    let postsHTML = "";
+    postsToDisplay.forEach(post => {
+        const postClass = getPostClassByCategory(post.categories);
+        postsHTML += `<div class="post ${postClass}" data-post-id="${post.id}">
+            <h3>${post.title}</h3>
+            <p>${post.content}</p>
+            <p>Category: ${post.categories}</p>
+            <p>Date: ${new Date(post.date).toLocaleDateString()}</p>
+            <p>Votes: ${post.votes}</p>
+        </div>`;
+    });
+    postsSection.innerHTML = postsHTML;
+}
 
 document.addEventListener("DOMContentLoaded", function() {
     const postsSection = document.querySelector('.posts-section');
-
-    const filters = document.querySelectorAll('[name="sort-order"]');
-
     const categoryFilter = document.querySelector('#category-filter');
+    const createPostForm = document.getElementById("createPostForm");
 
     function fetchAndDisplay(endpoint) {
         console.log("Fetching from:", endpoint);
         fetch(endpoint)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`HTTP error! Status: ${response.status}`);
-                }
-                return response.json();
-            })
+            .then(response => response.json())
             .then(data => {
                 posts = data;
                 displayPosts(posts);
             })
             .catch(error => {
-                console.log("Error fetching posts: ", error);
+                console.log("Error fetching posts:", error);
             });
     }
 
@@ -44,57 +54,23 @@ document.addEventListener("DOMContentLoaded", function() {
     function applyFilter() {
         const category = categoryFilter.value;
         const sortOrder = document.querySelector('[name="sort-order"]:checked').value;
-    
-        let endpoint = `${API_ENDPOINT}posts`; // default
-    
+
+        let endpoint = `${API_ENDPOINT}posts`; 
+
         if (category !== 'all') {
             endpoint = `${API_ENDPOINT}posts/order?category=${category}&sort=${sortOrder}`;
-
         } else {
-            endpoint = `${API_ENDPOINT}posts/${sortOrder}`
+            endpoint = `${API_ENDPOINT}posts/${sortOrder}`;
         }
-    
+
         fetchAndDisplay(endpoint);
     }
 
-    // Initial fetch and display
     fetchAndDisplay(`${API_ENDPOINT}posts`);
-    function getPostClassByCategory(category) {
-        switch (category) {
-            case 'graffiti':
-                return 'graffiti';
-            case 'Fly Tipping':
-                return 'fly-tipping';
-            case 'Noise Disturbance':
-                return 'noise-disturbance';
-            case 'anti-social-behaviour':
-                return 'anti-social-behaviour';
-            case 'Parking':
-                return 'parking';
-            case 'Damaged Property':
-                return 'damaged-property';
-            case 'Street Sweeping':
-                return 'street-sweeping';
-        }
-    }
-    function displayPosts(postsToDisplay) {
-        let postsHTML = "";
-        postsToDisplay.forEach(post => {
-            const postClass = getPostClassByCategory(post.categories)
-            postsHTML += `<div class="post ${postClass}" data-post-id="${post.id}">
-                <h3>${post.title}</h3>
-                <p>${post.content}</p>
-                <p>Category: ${post.categories}</p>
-                <p>Date: ${new Date(post.date).toLocaleDateString()}</p>
-                <p>Votes: ${post.votes}</p>
-
-            </div>`;
-        });
-        postsSection.innerHTML = postsHTML;
-    }
 
     const modal = document.getElementById("postModal");
     const closeModal = document.querySelector(".close");
+    const closeCreate = document.querySelector(".close-create");
 
     postsSection.addEventListener('click', function(e) {
         const postElement = e.target.closest('.post');
@@ -106,17 +82,25 @@ document.addEventListener("DOMContentLoaded", function() {
             modal.style.display = "block";
         }
     });
+    openPostModalBtn.addEventListener("click", function() {
+        createPostModal.style.display = "block";
+    });
+    
+    closeCreate.onclick = function() {
+        createPostModal.style.display = "none";
+    };
 
     closeModal.onclick = () => {
         modal.style.display = "none";
     };
-
     window.onclick = (event) => {
         if (event.target === modal) {
             modal.style.display = "none";
         }
+        if (event.target === createPostModal) {
+            createPostModal.style.display = "none";
+        }
     };
-
     document.getElementById("voteBtn").addEventListener("click", () => {
         if (typeof currentPostIndex !== 'undefined') {
             const currentPost = posts[currentPostIndex];
@@ -175,127 +159,105 @@ document.addEventListener("DOMContentLoaded", function() {
             });
         }
     });
-    
 
-    const openPostModalBtn = document.getElementById("openPostModalBtn");
-    const createPostModal = document.getElementById("createPostModal");
-    const closeCreate = document.querySelector(".close-create");
+    createPostForm.addEventListener("submit", function (event) {
+        event.preventDefault();
     
-    openPostModalBtn.addEventListener("click", function() {
-        createPostModal.style.display = "block";
+        const title = document.getElementById("postTitle").value;
+        const content = document.getElementById("postContent").value;
+        const category = document.getElementById("postCategory").value;
+    
+        const currentDate = new Date();
+        const dateOptions = {
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
+            hour12: false,
+        };
+        const localDateTime = currentDate.toLocaleString("en-GB", dateOptions);
+    
+        const postData = {
+            post_title: title,
+            post_content: content,
+            post_date: localDateTime,
+            post_categories: category,
+            post_votes: 0,
+            post_stage: 'Active'
+        };
+    
+        fetch(`${API_ENDPOINT}posts`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(postData),
+        })
+            .then(response => {
+                if (!response.ok) {
+                    console.error(`HTTP error! Status: ${response.status}`);
+                    return response.text(); 
+                }
+                return response.json();
+            })
+            .then(data => {
+                if (data) {
+                    console.log("Server response:", data);
+                    createPostModal.style.display = "none";
+    
+                    posts.push(data);
+                    displayPosts(posts);
+    
+                    document.getElementById("postTitle").value = "";
+                    document.getElementById("postContent").value = "";
+                    document.getElementById("postCategory").value;
+                    alert("New post successfully created!")
+                }
+            })
+            .catch(error => {
+                console.error("Error posting data to the server:", error);
+            });
+    });
+    const deleteButton = document.getElementById("deleteBtn");
+    deleteButton.addEventListener("click", () => {
+        if (typeof currentPostIndex !== 'undefined') {
+            const currentPost = posts[currentPostIndex];
+            const postIdToDelete = currentPost.id;
+            fetch(`${API_ENDPOINT}posts/${postIdToDelete}`, {
+                method: "DELETE",
+            })
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error(`HTTP error! Status: ${response.status}`);
+                    }
+
+                    alert("Post deleted successfully!");
+                    window.location.href = "/client/home.html";
+                })
+                .catch(error => {
+                    console.error("Error deleting the post:", error);
+                });
+        }
     });
 
-    closeCreate.onclick = function() {
-        createPostModal.style.display = "none";
-    };
-});
+    document.getElementById('logout').addEventListener('click', () => {
+        localStorage.clear();
+        window.location.assign('./login.html');
+    });
 
-createPostForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    const title = document.getElementById("postTitle").value;
-    const content = document.getElementById("postContent").value;
-    const category = document.getElementById("postCategory").value;
-
-    const currentDate = new Date();
-    const dateOptions = {
-        year: "numeric",
-        month: "2-digit",
-        day: "2-digit",
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
-        hour12: false,
-    };
-    const localDateTime = currentDate.toLocaleString("en-GB", dateOptions);
-
-    const postData = {
-        post_title: title,
-        post_content: content,
-        post_date: localDateTime,
-        post_categories: category,
-        post_votes: 0,
-        post_stage: 'Active'
-    };
-
-    fetch(`${API_ENDPOINT}posts`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(postData),
-    })
-        .then(response => {
-            if (!response.ok) {
-                console.error(`HTTP error! Status: ${response.status}`);
-                return response.text(); 
+    async function loadPosts() {
+        const options = {
+            headers: {
+                'Authorization': localStorage.getItem("token")
             }
-            return response.json();
-        })
-        .then(data => {
-            if (data) {
-                console.log("Server response:", data);
-                createPostModal.style.display = "none";
-
-                posts.push(data);
-                displayPosts(posts);
-
-                document.getElementById("postTitle").value = "";
-                document.getElementById("postContent").value = "";
-                document.getElementById("postCategory").value = "Graffiti";
-            }
-        })
-        .catch(error => {
-            console.error("Error posting data to the server:", error);
-        });
-});
-
-
-const deleteButton = document.getElementById("deleteBtn");
-
-deleteButton.addEventListener("click", () => {
-    console.log("Delete button clicked");
-    console.log("currentPostIndex:", currentPostIndex);
-   
-    if (typeof currentPostIndex !== 'undefined') {
-        const postIdToDelete = posts[currentPostIndex].post_id; 
-        fetch(`${API_ENDPOINT}posts/${postIdToDelete}`, {
-            method: "DELETE",
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            
-            alert("Post deleted successfully!");
-            window.location.href = "/client/home.html";
-        })
-        .catch(error => {
-            console.error("Error deleting the post:", error);
-        });
-    }
-});
-document.getElementById('logout').addEventListener('click', () => {
-    //localStorage.removeItem('token');
-    
-    localStorage.clear(); 
-    window.location.assign('./login.html')
-})
-
-
-async function loadPosts () {
-
-    const options = {
-        headers: {
-            'Authorization': localStorage.getItem("token")
+        };
+        const response = await fetch(`${API_ENDPOINT}posts`, options);
+        if (!options.headers.Authorization) {
+            window.location.assign("./login.html");
         }
     }
-    const response = await fetch(`${API_ENDPOINT}/posts`, options);
-    //console.log(options.headers.Authorization)
-    if (options.headers.Authorization) (console.log("You've successfully logged in!"))
-    else {
-        window.location.assign("./login.html");
-    }
-}
 
-loadPosts();
+    loadPosts();
+});
